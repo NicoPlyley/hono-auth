69cfb13cd6255bf4112edca41f944c7d546c1040
Nico Plyley <nico@phoenixtechlab.com>
2024-04-27 13:44:17 -0700

    :sparkles: Create drizzle middleware

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
 import { Hono } from 'hono';
+import { drizzle } from 'drizzle-orm/d1';
 
-const app = new Hono();
+type Bindings = {
+  DB: D1Database;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
+
+app.use(async (c, next) => {
+  const db = drizzle(c.env.DB);
+  c.set('db', db);
+
+  await next();
+});
 
 app.get('/', (c) => {
   return c.text('Hello Hono!');
