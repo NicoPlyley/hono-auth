6f79c731e0b50d8c3fc1eca874852fdec3426820
Nico Plyley <nico@phoenixtechlab.com>
2024-04-27 14:15:26 -0700

    :card_file_box: Created drizzle migration

---
+++ src/drizzle/migrations/0000_blue_loa.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `users` (
+	`id` integer PRIMARY KEY NOT NULL,
+	`name` text NOT NULL,
+	`username` text NOT NULL,
+	`password` text NOT NULL
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `users_username_unique` ON `users` (`username`);
