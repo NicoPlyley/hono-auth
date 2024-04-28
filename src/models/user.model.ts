72072840078f9405b2390dc35b572bd6108901b6
Nico Plyley <nico@phoenixtechlab.com>
2024-04-28 01:07:45 -0700

    :sparkles: Added error messages

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,12 +1,17 @@
 import { z } from 'zod';
 
 export const userModel = z.object({
-  name: z.string(),
-  username: z.string(),
-  password: z.string(),
+  name: z.string({ message: 'Name is required' }),
+  username: z
+    .string({ message: 'Username is required' })
+    .min(4, 'Username must contain at least 4 character')
+    .max(50, 'User must contain less than 50 character'),
+  password: z
+    .string({ message: 'Password is required' })
+    .min(6, 'Password must contain at least 6 character'),
 });
 
 export const loginModel = z.object({
-  username: z.string(),
-  password: z.string(),
+  username: z.string({ message: 'Username is required' }),
+  password: z.string({ message: 'Password is required' }),
 });
