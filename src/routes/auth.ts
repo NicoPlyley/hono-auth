eb11da4db7f5caeef47cac6018c03ef017fc58a8
Nico Plyley <nico@phoenixtechlab.com>
2024-04-27 18:33:38 -0700

    :sparkles: Created login and register routes

---
+++ src/routes/auth.ts
@@ -0,0 +1,39 @@
+import { Hono } from 'hono';
+import * as User from '../services/user';
+import { zValidator } from '@hono/zod-validator';
+import { loginModel, userModel } from '../models';
+
+type Bindings = {
+  JWT_SECRET: string;
+};
+
+const auth = new Hono<{ Bindings: Bindings }>();
+
+auth.post('/login', zValidator('json', loginModel), async (c) => {
+  let data = await c.req.json();
+  const db = c.get('db');
+
+  const token = await User.login(db, data, c.env.JWT_SECRET);
+
+  return c.json({
+    success: true,
+    token: token,
+  });
+});
+
+auth.post('/register', zValidator('json', userModel), async (c) => {
+  const data = await c.req.json();
+  const db = c.get('db');
+
+  const newUser = await User.create(db, data);
+
+  return c.json(
+    {
+      success: true,
+      data: newUser,
+    },
+    201
+  );
+});
+
+export default auth;
